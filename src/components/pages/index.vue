af134d088095b7741d1d7d85a0533d8c5d8dc90a
Shujinko53 <shujinko53@yandex.ru>
2022-09-26 17:25:22 +0300

    Создание проекта

---
+++ src/components/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :class="$style.TestName">
+        <div :class="$style.buttons">
+            <button :class="$style.button">Заказать в Москву</button>
+            <button :class="$style.button">Заказать в Санкт-Петербург</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestName',
+
+    components: {
+        //
+    },
+
+    props: {
+        //
+    },
+
+    data() {
+        return {
+            //
+        };
+    },
+
+    computed: {
+        //
+    },
+
+    methods: {
+        //
+    },
+};
+</script>
+
+<style lang="scss" module>
+    .TestName {
+        font-size: 1.4rem;
+        font-weight: 500;
+        line-height: 2rem;
+        color: $text-color;
+        cursor: default;
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
+
+module.exports = {
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: `@import "@/assets/scss/styles.scss";`,
+			},
+            css: {
+              // Примечание: формат конфигурации отличается между Vue CLI v4 и v3
+              // Для пользователей Vue CLI v3, обратитесь к документации css-loader v1
+              // https://github.com/webpack-contrib/css-loader/tree/v1.0.1
+              modules: {
+                localIdentName: '[local]-[hash:base64:8]'
+              },
+            }
+		},
+	}
+};
